006624d2a4dffeddacb337ebe5f86704760b734c
Goldenprevue <goldenprevue@gmail.com>
2023-05-29 13:29:46 +0900

    .css path update

--- max.js
+++ max.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
+const path = require('path');
 
 const styleCode = `
-html, body, div, span, iframe,
-h1, h2, h3, h4, h5, h6, p, pre, code, a,i, 
- ol, ul, li,
- form, label,
-table, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, 
-figure, footer, header, 
-menu, nav, output, section, sum,
- audio, video {
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
 	margin: 0;
 	padding: 0;
 	border: 0;
@@ -17,9 +22,9 @@ menu, nav, output, section, sum,
 	font: inherit;
 	vertical-align: baseline;
 }
-
-article, aside, details, figure, 
-footer, header, menu, nav, section {
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
 	display: block;
 }
 body {
@@ -28,13 +33,20 @@ body {
 ol, ul {
 	list-style: none;
 }
-
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
 table {
 	border-collapse: collapse;
 	border-spacing: 0;
 }`;
-
-fs.writeFile('global.css', styleCode, (err) => {
+const filePath = path.join(__dirname, 'src', 'global.css');
+fs.writeFile(filePath, styleCode, (err) => {
       if (err) {
             console.error('Error creating style file:', err);
       } else {
